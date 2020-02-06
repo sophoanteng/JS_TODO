472ceec75f4337f371c9510eb5556624a7a5ba75
sokphorn.teng <sokphorn.teng@PNC.LAN>
2020-02-06 20:01:56 +0700

    todo4

--- todo4/js/app.js
+++ todo4/js/app.js
@@ -7,14 +7,24 @@ const dayAndMonth = {
 };
 dateElement.innerHTML =new Date().toLocaleString('en-Us', dayAndMonth);
 
-
-
 const inputItem = document.querySelector('#input');
+const listItem = document.querySelector('#list');
+
 document.addEventListener('keydown', event => {
     if(event.key === "Enter"){
        const toDo = inputItem.value;
-       console.log(toDo);
-       inputItem.value = "";
-       
+       if(toDo){
+        insertFunction(toDo);
+     }
+    inputItem.value = "";
     }
 });
+
+function insertFunction(toDo){
+    const result =`
+    <li class="item">
+      <p class="text">${toDo}</p>
+    </li>
+ `;
+listItem.insertAdjacentHTML('beforeend', result);
+}
