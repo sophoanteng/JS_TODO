54130acaf3426e0f4d0de008131505d9dc8fa91a
sokphorn.teng <sokphorn.teng@PNC.LAN>
2020-02-06 20:09:11 +0700

    todo6

--- todo6/js/app.js
+++ todo6/js/app.js
@@ -10,21 +10,26 @@ dateElement.innerHTML =new Date().toLocaleString('en-Us', dayAndMonth);
 const inputItem = document.querySelector('#input');
 const listItem = document.querySelector('#list');
 
+const check = "fa-check-circle";
+const uncheck = "fa-circle-thin";
+const line = "lineThrough";
 document.addEventListener('keydown', event => {
     if(event.key === "Enter"){
        const toDo = inputItem.value;
        if(toDo){
-        insertFunction(toDo);
+        insertFunction(toDo, true);
      }
     inputItem.value = "";
     }
 });
 
-function insertFunction(toDo){
+function insertFunction(todo, isDone){
+    const isCheck = isDone ? check : uncheck;
+    const isLine = isDone ? line : "";
     const result =`
     <li class="item">
-       <i class="fa fa-check-circle co" aria-hidden="true" job="tist"></i>
-       <p class="text">${toDo}</p>
+       <i class="fa  ${isCheck} co"  job="tisk"></i>
+       <p class="text ${isLine}">${todo}</p>
        <i class="fa fa-trash-o de" job="delete"></i>
     </li>
  `;
